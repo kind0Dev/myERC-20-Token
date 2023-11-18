41a996db8a5f879a7d78d7f15137a619d971eec6
RAJI <kind0devpro@gmail.com>
2023-11-18 01:23:23 -0800

    Update DeployScript for MyToken

---
+++ script/DeployMyToken.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DeployMyToken is Script {
+    uint256 public constant INITIAL_SUPPLY = 10000 ether;
+    function run() external {
+        vm.startBroadcast();
+        MyToken myToken = new MyToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+    }
+}
--- src/MyToken.sol
+++ src/MyToken.sol
@@ -2,6 +2,14 @@
 
 pragma solidity ^0.8.19;
 
-contract MyToken{
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+
+    constructor (uint256 initialSupply) ERC20("MyToken", "MT"){
+        _mint(msg.sender, initialSupply);
+    }
+    function name() public override pure returns (string memory) {
+        return "Manual Token";
+    }
 }
