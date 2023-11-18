1bfbac878b68a759b83979b988a23441556e8e6f
RAJI <kind0devpro@gmail.com>
2023-11-18 05:17:26 -0800

    test

--- test/MyTokenTest.t.sol
+++ test/MyTokenTest.t.sol
@@ -6,16 +6,14 @@ import {Test} from "forge-std/Test.sol";
 import {DeployMyToken} from "../script/DeployMyToken.s.sol";
 import {MyToken} from "../src/MyToken.sol";
 
-
 contract MyTokenTest is Test {
     MyToken public myToken;
     DeployMyToken public deployer;
 
     address bob = makeAddr("bob");
-    address alice =  makeAddr("alice");
+    address alice = makeAddr("alice");
 
     uint256 public constant STARTING_BALANCE = 100 ether;
-
 
     function setUp() public {
         deployer = new DeployMyToken();
@@ -23,10 +21,25 @@ contract MyTokenTest is Test {
 
         vm.prank(msg.sender);
         myToken.transfer(bob, STARTING_BALANCE);
-
     }
 
     function testBobBalance() public {
         assertEq(STARTING_BALANCE, myToken.balanceOf(bob));
+    }
+
+    function testAllowance() public {
+        uint256 initialAllowance = 1000;
+
+        // Bob approves Alice to spend tokens on her behalf
+        vm.prank(bob);
+        myToken.approve(alice, initialAllowance);
+
+        uint256 transferAmount = 500;
+
+        vm.prank(alice);
+        myToken.transferFrom(bob, alice, transferAmount);
+
+        assertEq(STARTING_BALANCE - transferAmount, myToken.balanceOf(bob));
+        assertEq(transferAmount, myToken.balanceOf(alice));
     }
 }
